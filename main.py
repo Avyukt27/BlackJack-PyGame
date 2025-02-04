55e3a49208e156ce2e1f3b54547ffb5c8481f894
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-02-04 19:30:21 +0530

    added draw card function

--- main.py
+++ main.py
@@ -3,27 +3,35 @@ import requests
 from io import BytesIO
  
 pygame.init()
-screen = pygame.display.set_mode((800, 600))
+window = pygame.display.set_mode((800, 600))
 
 deck_id = requests.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").json()["deck_id"]
 
 class Card:
-    def __init__(self, response: requests.Response):
+    def __init__(self, response: requests.Response) -> None:
         self.position = pygame.Vector2(0, 0)
         self.img = pygame.image.load(BytesIO(requests.get(response.json()["cards"][0]["image"]).content))
         self.img = pygame.transform.scale(self.img, (100, 155))
         self.rect = self.img.get_rect()
     
-    def draw(self, window: pygame.Surface):
+    def draw(self, window: pygame.Surface) -> None:
         window.blit(self.img, self.position)
 
-response = requests.get(f"https://www.deckofcardsapi.com/api/deck/{deck_id}/draw/?count=1")
+def draw_card(deck_id: str) -> requests.Response:
+    return requests.get(f"https://www.deckofcardsapi.com/api/deck/{deck_id}/draw/?count=1")
 
-card = Card(response)
+def update_screen(window: pygame.Surface) -> None:
+    card.draw(window)
+    pygame.display.update()
 
-while True:
+card = Card(draw_card(deck_id))
+
+run = True
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-    card.draw(screen)
-    pygame.display.update()
+            run = False
+    update_screen(window)
+
+pygame.quit()
+exit()
