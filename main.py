bc2cd9f322c2d48e9c182996ca8ce1b9e0dbc266
Avyukt27 <avyukt.aggarwal007@gmail.com>
2025-02-04 19:26:32 +0530

    Created card class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pygame
 import requests
-import sys
 from io import BytesIO
  
 pygame.init()
@@ -8,14 +7,23 @@ screen = pygame.display.set_mode((800, 600))
 
 deck_id = requests.get("https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").json()["deck_id"]
 
+class Card:
+    def __init__(self, response: requests.Response):
+        self.position = pygame.Vector2(0, 0)
+        self.img = pygame.image.load(BytesIO(requests.get(response.json()["cards"][0]["image"]).content))
+        self.img = pygame.transform.scale(self.img, (100, 155))
+        self.rect = self.img.get_rect()
+    
+    def draw(self, window: pygame.Surface):
+        window.blit(self.img, self.position)
+
 response = requests.get(f"https://www.deckofcardsapi.com/api/deck/{deck_id}/draw/?count=1")
-response = requests.get(response.json()["cards"][0]["image"])
-img = pygame.image.load(BytesIO(response.content))
- 
+
+card = Card(response)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            sys.exit()
-    screen.blit(img, (0, 0))
+    card.draw(screen)
     pygame.display.update()
